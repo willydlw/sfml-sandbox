b0045b0799c45c7983317c3424c4dc2464c968f3
Diane Williams <diane.williams@ucdenver.edu>
2025-01-30 18:13:02 -0700

    removing rotateRect

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # sfml-sandbox
 place to experiment with sfml
+
+
+
+## Build 
+
+```
+mkdir build
+cd build
+cmake <path-to-cloned-repo>
+cmake --build .
+./src/imgui-sfml-example # or .\src\Debug\imgui-sfml-example.exe if you're on Windows
+```◊
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,45 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+#include "imgui-SFML.h"
+#include "imgui.h"
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
 
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        ImGui::ShowDemoWindow();
+
         window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
         window.display();
     }
+
+    ImGui::SFML::Shutdown();
+
+    return 0;
 }
