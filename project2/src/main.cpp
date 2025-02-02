56913c55c5bf5b719c9288c45e5e58e0504522d7
Diane Williams <diane.williams@ucdenver.edu>
2025-02-01 18:03:00 -0700

    simple listbox in project2

--- project2/src/main.cpp
+++ project2/src/main.cpp
@@ -2,10 +2,28 @@
 #include <imgui-SFML.h>
 #include <imgui.h>
 
+void MyGUIWindow(){
+    // store currently selected item
+    int currentItem = 0;
+
+    // start drawing the window
+    ImGui::Begin("ListBox Window");
+
+    // list of items to display
+    const char* items[] = {"Item 1", "Item 2", "Item 3"};
+    
+    // create list box
+    ImGui::ListBox("Select an item\n", &currentItem, items, IM_ARRAYSIZE(items) );
+
+    ImGui::End(); // finish drawing window
+}
+
 int main()
 {
+    bool listEnabled = true; 
+
     auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    window.setFramerateLimit(60);
     if (!ImGui::SFML::Init(window))
         return -1;
 
@@ -24,11 +42,9 @@ int main()
 
         ImGui::SFML::Update(window, clock.restart());
 
-        ImGui::Begin("Hello, world!");
-        ImGui::Button("Look at this pretty button");
-        ImGui::End();
-
-        ImGui::ShowDemoWindow();
+        if(listEnabled){
+            MyGUIWindow();
+        }
 
         window.clear();
         ImGui::SFML::Render(window);
