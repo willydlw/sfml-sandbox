ad145154586b2720b7bdef1bdbcc01b6287d5b3e
willydlw <willydlw@comcast.net>
2025-03-04 09:35:57 -0700

    changed font path

--- gameOfLife/test/main.cpp
+++ gameOfLife/test/main.cpp
@@ -1,7 +1,20 @@
 #include "neighborTest.hpp"
 
+#include <SFML/Graphics/Font.hpp>
+#include <iostream>
+#include <filesystem>
+
 int main(void)
 {
+    std::string fontFile{"arial.ttf"};
+
+    sf::Font font;
+    if(!font.openFromFile(fontFile))
+    {
+        std::cerr << "[FATAL] failure to open font file: " << fontFile << "\n";
+        std::cerr << "Current working directory: " << std::filesystem::current_path() << "\n";
+        return 1;
+    }
     neighborTest::runTests();
     return 0;
 }
