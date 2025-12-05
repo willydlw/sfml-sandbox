5cab4d42d880b23674283118530966662ade92a5
Diane Williams <diane.williams@ucdenver.edu>
2025-12-05 14:58:34 -0700

    Drawing one Santa Sprite which needs to be scaled to a smaller size

--- animate/src/main.cpp
+++ animate/src/main.cpp
@@ -1,9 +1,26 @@
 #include <SFML/Graphics.hpp>
 
+#include <iostream>
+#include <optional>
+
 int main()
 {
     // create the window
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Santa Animation");
+
+    sf::RectangleShape ground{{800, 100}};
+    ground.setPosition({0, 500});
+    ground.setFillColor(sf::Color::Green);
+
+    sf::Texture santaTexture;
+    if(!santaTexture.loadFromFile("images/Walk1.png")){
+        std::cerr << "Failed to load image\n";
+    }
+
+    sf::Sprite santaSprite(santaTexture);
+    
+    sf::Vector2u tsize = santaTexture.getSize();
+    std::cerr << "texture size, x: " << tsize.x << ", y: " << tsize.y << "\n";
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -17,10 +34,12 @@ int main()
         }
 
         // clear the window with black color
-        window.clear(sf::Color::Black);
+        window.clear(sf::Color::White);
 
         // draw everything here...
-        // window.draw(...);
+        window.draw(ground);
+
+        window.draw(santaSprite);
 
         // end the current frame
         window.display();
